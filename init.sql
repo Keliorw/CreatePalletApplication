f51eaa38dbd682061e34aac2efd58a18d25c17c2
keliorw <miabrio228@gmail.com>
2024-01-19 19:13:21 +0300

    Немного отредактировал файлы для докера

--- init.sql
+++ init.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE create_pallet_application_development;
-CREATE USER create_pallets_admin WITH password "QO1e#:YU&.8t4l";
+CREATE USER create_pallets_admin WITH password 'QO1e:YU&.8t4l';
 GRANT ALL privileges ON DATABASE create_pallet_application_development TO create_pallets_admin;
